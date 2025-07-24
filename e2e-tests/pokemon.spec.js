8184856562b02e45b040415a352a6062106c26a5
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2025-07-24 16:16:50 +0200

    11.9 added final test

--- e2e-tests/pokemon.spec.js
+++ e2e-tests/pokemon.spec.js
@@ -7,3 +7,13 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+
+describe('Pokemon page', () => {
+  test('Checking Charmander page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('charmander').click()
+    await expect(page.getByText('charmander')).toBeVisible()
+    await expect(page.getByText('solar power')).toBeVisible()
+    await expect(page.getByText('blaze')).toBeVisible()
+  })
+})
